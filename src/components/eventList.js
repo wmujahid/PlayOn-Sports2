cd12fd84b6a541c4fe70d7beb915e08f1135832b
wmujahid <wmujahid47@gmail.com>
2021-02-20 05:44:31 -0500

    changed to material-ui library

--- src/components/eventList.js
+++ src/components/eventList.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect }from 'react';
 import axios from 'axios';
-import { Form, Col, Table } from 'react-bootstrap';
+import { Table, TableCell, TableRow, TableHead, TableBody, FormLabel, TextField, FormControl, Select } from '@material-ui/core';
 import moment from 'moment';
 
 function format(dateString) {
@@ -12,7 +12,7 @@ function EventList() {
   const [startDate, setStartDate] = useState('');
   const [endDate, setEndDate] = useState('');
   const [eventData, setEventData] = useState([]);
-  
+
   useEffect(async () => {
     const result = await axios.get(
       `https://challenge.nfhsnetwork.com/v2/search/events/upcoming?state_association_key=${associationKey}&size=50&from=${startDate}&to=${endDate}&start=0`
@@ -22,70 +22,62 @@ function EventList() {
 
   return (
     <div>
-      <div>
-        <Form>
-          <Form.Row>
-            <Col lg={2}>
-              <Form.Label>
-                State Association
-                <Form.Control
-                  as="select"
-                  name=""
-                  onChange={(event) => setAssociationKey(event.target.value)}
-                >
-                  <option value='18bad24aaa'>GHSA</option>
-                  <option value='542bc38f95'>UIL</option>
-                </Form.Control>
-              </Form.Label>
-            </Col>
-            <Col sm="auto">
-              <Form.Label>
-                Start Date: 
-                <Form.Control
-                  type="datetime-local"
-                  name="startDate"
-                  onChange={({ target: { value } }) => setStartDate(value && (new Date(value)).toISOString())}
-                  placeholder="Start Date"
-                />
-              </Form.Label>
-            </Col>
-            <Col sm="auto">
-              <Form.Label>
-                End Date: 
-                <Form.Control
-                  type="datetime-local"
-                  name="endDate"
-                  onChange={({ target: { value } }) => setEndDate(value && (new Date(value)).toISOString())}
-                  placeholder="End Date"
-                />
-              </Form.Label>
-            </Col>
-          </Form.Row>
-        </Form>
-      </div>
-      <Table striped bordered='true' hover>
-        <thead>
-          <tr>
-            <th>Key</th>
-            <th>Headline</th>
-            <th>Subheadline</th>
-            <th>Start Time</th>
-          </tr>
-        </thead>
+      <form>
+        <FormLabel>State Association</FormLabel> 
+          <FormControl component="fieldset">
+            <Select
+              label="End Date"
+              native
+              autoWidth='true'
+              name="stateAssociationKey"
+              onChange={(event) => setAssociationKey(event.target.value)}
+            >
+              <option aria-label="None" value="" />
+              <option value='18bad24aaa'>GHSA</option>
+              <option value='542bc38f95'>UIL</option>
+            </Select>
+          </FormControl>
+          <TextField
+            label="Start Date"
+            type="datetime-local"
+            onChange={({ target: { value } }) => setStartDate(value && (new Date(value)).toISOString())}
+            InputLabelProps={{
+              shrink: true,
+            }}
+          /> 
+          <TextField
+            label="End Date"
+            type="datetime-local"
+            onChange={({ target: { value } }) => setEndDate(value && (new Date(value)).toISOString())}
+            InputLabelProps={{
+              shrink: true,
+            }}
+          /> 
+      </form>
+
+      <Table stickyHeader striped bordered='true' hover>
+        <TableHead>
+          <TableRow>
+            <TableCell>Key</TableCell>
+            <TableCell>Headline</TableCell>
+            <TableCell>Subheadline</TableCell>
+            <TableCell>Start Time</TableCell>
+          </TableRow>
+        </TableHead>
         {eventData.map(({ key, start_time, publishers: [{ broadcasts: [{ headline, subheadline }] }] }) => {
           console.log(key, start_time);
           return (
-            <tbody key={key}>
-              <tr>
-                <td>{key}</td>
-                <td>{headline}</td>
-                <td>{subheadline}</td>
-                <td>{format(start_time)}</td>
-              </tr>
-            </tbody>
+            <TableBody key={key}>
+              <TableRow hover>
+                <TableCell>{key}</TableCell>
+                <TableCell>{headline}</TableCell>
+                <TableCell>{subheadline}</TableCell>
+                <TableCell>{format(start_time)}</TableCell>
+              </TableRow>
+            </TableBody>
             )
           })}
-          </Table>
+      </Table>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,10 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+  palette: {
+    type: "dark",
+  }
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
